5574a85fce3e24407d723ee53686949ddad0ec58
Th-Svn <thelma.sevenier14@gmail.com>
2022-05-04 11:18:35 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -111,7 +111,13 @@ else {
 };
     }
 
-for(var i=0; i < enemyNames.length; i++) {
+var startGame = function () {
+    // reset player stats 
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+
+    for(var i=0; i < enemyNames.length; i++) {
     if (playerHealth > 0) {
     // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert("Welcome to Robot Gladiators! Round " + (i+1));
@@ -134,6 +140,31 @@ for(var i=0; i < enemyNames.length; i++) {
         break;
     }
 }
+    // after the loop ends, player is either out of health or enemies to fight, so run the endGame function
+    endGame();
+    };
+
+// function to end the game 
+
+var endGame = function() {
+    // if player is still alive, player wins !
+    if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+    // ask player if they'd like to play again
+    var playerAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playAgainConfirm) {
+        // restart the game
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
 
 console.log("This logs a string, good for leaving yourself a message");
 
